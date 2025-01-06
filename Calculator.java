e749a8aa3a4138131a6be2f89aa7ba8c132b4e8b
vivek pathak <vivekkpaathak@gmail.com>
2025-01-06 21:27:18 +0530

    added cal

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator{
+
+public static void main(String [] args)
+{
+          System.out.println("welcome to calculator !");
+          CalcMethodAdd obj= new CalcMethodAdd();
+        int cha= obj. addition(5,7);
+         System.out.println(cha);
+
+
+}
+
+}
